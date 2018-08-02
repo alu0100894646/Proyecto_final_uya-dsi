93c1ff4d92f07254c4bee43110e9f3c51ed73c8c
unknown <alu0100894646@ull.edu.es>
2018-08-02 16:56:06 +0100

    Comprobando cosas

--- public/js/server.js
+++ public/js/server.js
@@ -4,11 +4,16 @@ var path = require('path');
 var port = 3000;
 
 //app.use(express.static('public'));
-app.use('/static', express.static(__dirname + '../public'));
+app.use('/static', express.static('/public/'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'../public','index.html'));
+    res.sendFile(path.join(__dirname,'../','index.html'));
 })
+
+/*app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'materialize.js'));
+    res.sendFile(path.join(__dirname, 'init.js'));
+})*/
 
 app.listen(port, (err) => {
     if (err) {
