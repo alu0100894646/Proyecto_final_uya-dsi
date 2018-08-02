d85030f354caed2972d0f74116695940c5220301
unknown <alu0100894646@ull.edu.es>
2018-08-02 15:22:38 +0100

    server.js file

--- js/server.js
+++ js/server.js
@@ -1,1 +1,18 @@
-// JavaScript source code
+var express = require('express');
+var app = express();
+var path = require('path');
+var port = 3000;
+
+//app.use(express.static('public'));
+app.use('/static', express.static(__dirname + '../public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'../public','index.html'));
+})
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('Ha ocurrido un error', err);
+    }
+    console.log('Server escuchando en ' + port);
+})
