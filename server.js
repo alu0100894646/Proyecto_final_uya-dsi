9a0f581d10fd1a5415e85d106ac74fed2f630dc2
alu0100881165 <alu0100881165@ull.edu.es>
2018-08-04 18:15:25 +0100

    Host express Funcionando
    Pagina hosteada server local usando node.js y express

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ var port = 3000;
 
 //app.use("/public", express.static(path.resolve(__dirname,'../', 'public')));
 //app.use(express.static('public'));
-app.use('/static', express.static('/public/'));
+app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public/css'));
+// app.use(express.static(__dirname + '/public/js'));
+// app.use(express.static(__dirname + '/public/img'));
+// app.use(express.static(__dirname + '/../css'));
+// app.use(express.static(__dirname + '/../js'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname,'../../', 'index.html');
+    res.sendFile(__dirname + '/public/index.html');
+    // res.sendFile(__dirname + '/public/css/materialize.css');
+    // res.sendFile(__dirname + '/public/css/style.css');
     //res.sendFile(path.join(__dirname, '../img/', 'study_large_table.jpg'));
 })
 
