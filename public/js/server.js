cff53cd90a3ae9895ba9aaeb4ebb18dd591e2fdf
unknown <alu0100894646@ull.edu.es>
2018-08-04 17:30:19 +0100

    pruebas ç

--- public/js/server.js
+++ public/js/server.js
@@ -3,11 +3,13 @@ var app = express();
 var path = require('path');
 var port = 3000;
 
+//app.use("/public", express.static(path.resolve(__dirname,'../', 'public')));
 //app.use(express.static('public'));
 app.use('/static', express.static('/public/'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'../','index.html'));
+    res.sendFile(__dirname,'../../', 'index.html');
+    //res.sendFile(path.join(__dirname, '../img/', 'study_large_table.jpg'));
 })
 
 /*app.get('/js', (req, res) => {
@@ -19,5 +21,6 @@ app.listen(port, (err) => {
     if (err) {
         return console.log('Ha ocurrido un error', err);
     }
+    console.log(__dirname);
     console.log('Server escuchando en ' + port);
 })
